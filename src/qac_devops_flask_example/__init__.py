2b7c5c0231502657f6ebff9c2702876a3a561d8e
bob-crutchley <bob.crutchley96@gmail.com>
2019-12-06 13:06:17 +0000

    renamed package

--- src/qac_devops_flask_example/__init__.py
+++ src/qac_devops_flask_example/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +6,4 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def root():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
 
